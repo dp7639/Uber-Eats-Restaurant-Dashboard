d262c2614a6ecf9814a20ee43b07500e49d80cc8
dp7639 <dp736116@gmail.com>
2022-08-19 03:22:39 +0530

    build

--- src/modules/Orders/index.js
+++ src/modules/Orders/index.js
@@ -66,7 +66,7 @@ const Orders = () => {
       title: "Price",
       dataIndex: "total",
       key: "total",
-      render: (price) => `${price}`,
+      render: (price) => `₹ ${price}`,
     },
     {
       title: "Status",
